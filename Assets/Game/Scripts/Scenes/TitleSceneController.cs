d7bfb44a414b6acbacebb74e778832ce3bb0b919
Karunia Ramadhan <rukanishino@gmail.com>
2014-02-14 17:02:38 +0700

    Title, folder restructure, UI assets

---
+++ Assets/Game/Scripts/Scenes/TitleSceneController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleSceneController : MonoBehaviour 
+{
+	public float WaitDuration = 2f;
+
+	// Use this for initialization
+	void Start() 
+	{
+		StartCoroutine(GoToMain());
+	}
+
+	IEnumerator GoToMain()
+	{
+		yield return new WaitForSeconds(WaitDuration);
+
+		Application.LoadLevel("MainScene");
+	}
+}
